daa85e28250413e0c6bf514ee6557e7b75e4f7bc
jasontek <jasontek2019@outlook.com>
2024-08-24 12:41:42 +0800

    [XMC7200]: add LED2 and LED3 to application,but it doesn't work.

--- bsp/Infineon/xmc7200-kit_xmc7200_evk/applications/main.c
+++ bsp/Infineon/xmc7200-kit_xmc7200_evk/applications/main.c
@@ -13,16 +13,30 @@
 
 #include "drv_gpio.h"
 
-#define LED_PIN     GET_PIN(16, 1)
+#define LED1_PIN     GET_PIN(16, 1)
+#define LED2_PIN     GET_PIN(16, 2)
+#define LED3_PIN     GET_PIN(16, 3)
+
 
 int main(void)
 {
-    rt_pin_mode(LED_PIN, PIN_MODE_OUTPUT);
+    rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
+    rt_pin_mode(LED2_PIN, PIN_MODE_OUTPUT);
+    rt_pin_mode(LED3_PIN, PIN_MODE_OUTPUT);
     for (;;)
     {
-        rt_pin_write(LED_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
-        rt_pin_write(LED_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
+        rt_pin_write(LED1_PIN, PIN_HIGH);
+        rt_thread_mdelay(200);
+        rt_pin_write(LED2_PIN, PIN_HIGH);
+        rt_thread_mdelay(200);
+        rt_pin_write(LED3_PIN, PIN_HIGH);
+        rt_thread_mdelay(200);
+        
+        rt_pin_write(LED1_PIN, PIN_LOW);
+        rt_thread_mdelay(200);
+        rt_pin_write(LED2_PIN, PIN_LOW);
+        rt_thread_mdelay(200);
+        rt_pin_write(LED3_PIN, PIN_LOW);
+        rt_thread_mdelay(200);
     }
 }
